8a33019bbef5d5812181b8e4d0546a0ffcb39690
Saeid <saeid.dadkhah@live.com>
2019-09-26 16:27:33 +0100

    clean setup and add exec access to setup

--- setup_zero.sh
+++ setup_zero.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo Starting clone
+cd ~
 apt-get install -y git
 git clone https://github.com/SaeidDadkhah/mfp.git
 echo Clone finished
@@ -11,5 +12,6 @@ echo Copy site files finished
 
 echo Starting to run main setup
 cd mfp
+chmod +x setup.sh
 setup.sh
 echo Main setup finished

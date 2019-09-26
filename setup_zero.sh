6da7b75a3ea81be4f1ecaa17e5f3aefbd12c2e89
Saeid <saeid.dadkhah@live.com>
2019-09-26 16:01:54 +0100

    finished setup.zero and init setup.sh

--- setup_zero.sh
+++ setup_zero.sh
@@ -3,5 +3,8 @@
 echo Starting clone
 apt-get install -y git nginx
 git clone https://github.com/SaeidDadkhah/mfp.git
+echo Clone finished
 
-cd mfp
+echo Starting to run main setup
+mfp/setup.sh
+echo Main setup finished

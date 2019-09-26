0c748f674cddb0a5cf3ecce743ae3dc80be33d67
Saeid <saeid.dadkhah@live.com>
2019-09-26 16:56:45 +0100

    added shadowsocks script and file downloading, and minimized setup.zero.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
 echo Starting installing packages
-apt-get install -y nginx
+export LC_ALL=C
+apt-get install -y nginx python python-pip libsodium-dev htop
 echo Installing packages finished
+
+echo Starting installing shadowsocks
+pip install shadowsocks
+pip install https://github.com/shadowsocks/shadowsocks/archive/master.zip -U
+ssserver -p 31066 -k "2wsxWSX2WSXwsx" -m "chacha20-ietf-poly1305" -d start
+echo Installing shadowsocks finished
+
+echo Starting daemonizing shadowsocks
+cp myscript /etc/init.d/
+chmod ugo+x /etc/init.d/myscript
+update-rc.d myscript defaults
+echo Daemonizing shadowsocks finished
+
+echo Starting copy site files
+wget http://us.download.nvidia.com/Windows/436.30/436.30-notebook-win10-64bit-international-dch-whql.exe
+mv 436.30-notebook-win10-64bit-international-dch-whql.exe pages/
+cp pages/* /var/www/
+echo Copy site files finished
 
 echo Starting configuring nginx
 rm /etc/nginx/sites-available/default
--- setup_zero.sh
+++ setup_zero.sh
@@ -6,10 +6,6 @@ apt-get install -y git
 git clone https://github.com/SaeidDadkhah/mfp.git
 echo Clone finished
 
-echo Starting copy site files
-cp pages/* /var/www/
-echo Copy site files finished
-
 echo Starting to run main setup
 cd mfp
 chmod +x setup.sh

36197c35c7552fc62396170daeb26e1f6843a456
Saeid <saeid.dadkhah@live.com>
2019-09-26 16:24:50 +0100

    copy site files

--- setup_zero.sh
+++ setup_zero.sh
@@ -5,6 +5,10 @@ apt-get install -y git
 git clone https://github.com/SaeidDadkhah/mfp.git
 echo Clone finished
 
+echo Starting copy site files
+cp pages/* /var/www/
+echo Copy site files finished
+
 echo Starting to run main setup
 cd mfp
 setup.sh

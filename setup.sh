25ea167ced22e31beaacad8d9338210a0b2ead3d
Saeid <saeid.dadkhah@live.com>
2019-09-26 16:21:50 +0100

    conf nginx

--- setup.sh
+++ setup.sh
@@ -3,3 +3,9 @@
 echo Starting installing packages
 apt-get install -y nginx
 echo Installing packages finished
+
+echo Starting configuring nginx
+rm /etc/nginx/sites-available/default
+mv nginx.conf /etc/nginx/nginx.conf
+service nginx restart
+echo Configuring nginx finished
